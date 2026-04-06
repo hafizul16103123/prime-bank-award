ed236e8db0a5eb69db541f3b82e88b3e2a021af7
neamulsabbir <neamulkabir97@gmail.com>
2026-04-07 01:15:18 +0600

    updated

--- src/components/organisms/Dashboard/Winners/WinnerLists.tsx
+++ src/components/organisms/Dashboard/Winners/WinnerLists.tsx
@@ -1,129 +1,26 @@
 "use client";
 import { ReusableTable } from "@/components/molecules";
 import { TableRow } from "@/components/ui/table";
+import { AdminStudentListItem } from "@/libes/interface/registration";
 import { useState } from "react";
 import { StudentRegistrationSheet } from "../../common";
-import type { Registration } from "./winner.types";
 import { WinnerTableRow } from "./WinnerTableRow";
-
-export type { Registration } from "./winner.types";
 
 const registrationTableHeader = ["Student", "Phone", "School", "ID", "Level", "Submitted", "Status", "Actions"];
 
-const mockData: Registration[] = [
-	{
-		id: "1",
-		student: "Shahriar Rahman",
-		phone: "01929459195",
-		school: "Green Hill High",
-		studentId: "32116464",
-		level: "A-Level",
-		submitted: "12 April 2026",
-		status: "YET_TO_AWARD",
-	},
-	{
-		id: "2",
-		student: "Zayan Hossain",
-		phone: "01929459195",
-		school: "Green Hill High",
-		studentId: "32116464",
-		level: "O-Level",
-		submitted: "12 April 2026",
-		status: "YET_TO_AWARD",
-	},
-	{
-		id: "3",
-		student: "Misha Akter",
-		phone: "01929459195",
-		school: "Green Hill High",
-		studentId: "32116464",
-		level: "A-Level",
-		submitted: "12 April 2026",
-		status: "YET_TO_AWARD",
-	},
-	{
-		id: "4",
-		student: "Priya Sen",
-		phone: "01929459195",
-		school: "Green Hill High",
-		studentId: "32116464",
-		level: "A-Level",
-		submitted: "12 April 2026",
-		status: "YET_TO_AWARD",
-	},
-	{
-		id: "5",
-		student: "Tanvir Ahmed",
-		phone: "01929459195",
-		school: "Green Hill High",
-		studentId: "32116464",
-		level: "O-Level",
-		submitted: "12 April 2026",
-		status: "YET_TO_AWARD",
-	},
-	{
-		id: "6",
-		student: "Sadia Islam",
-		phone: "01929459195",
-		school: "Green Hill High",
-		studentId: "32116464",
-		level: "O-Level",
-		submitted: "12 April 2026",
-		status: "AWARDED",
-	},
-	{
-		id: "7",
-		student: "Shahriar Rahman",
-		phone: "01929459195",
-		school: "Green Hill High",
-		studentId: "32116464",
-		level: "O-Level",
-		submitted: "12 April 2026",
-		status: "AWARDED",
-	},
-	{
-		id: "8",
-		student: "Shahriar Rahman",
-		phone: "01929459195",
-		school: "Green Hill High",
-		studentId: "32116464",
-		level: "A-Level",
-		submitted: "12 April 2026",
-		status: "AWARDED",
-	},
-	{
-		id: "9",
-		student: "Shahriar Rahman",
-		phone: "01929459195",
-		school: "Green Hill High",
-		studentId: "32116464",
-		level: "A-Level",
-		submitted: "12 April 2026",
-		status: "AWARDED",
-	},
-	{
-		id: "10",
-		student: "Shahriar Rahman",
-		phone: "01929459195",
-		school: "Green Hill High",
-		studentId: "32116464",
-		level: "O-Level",
-		submitted: "12 April 2026",
-		status: "AWARDED",
-	},
-];
 type Props = {
-	data?: Registration[];
+	data: AdminStudentListItem[];
 	emptyMessage?: string;
 	isLoading?: boolean;
+	updateData: () => Promise<void>;
 };
-
-export const WinnerLists = ({ data = mockData, emptyMessage, isLoading = false }: Props) => {
-	const [selectedStudent, setSelectedStudent] = useState<Registration | null>(null);
+export const WinnerLists = ({ data, emptyMessage, isLoading = false, updateData }: Props) => {
+	const [selectedStudent, setSelectedStudent] = useState<AdminStudentListItem | null>(null);
 	return (
 		<div>
 			<StudentRegistrationSheet
 				student={selectedStudent}
+				updateData={updateData}
 				onOpenChange={(next) => {
 					if (!next) setSelectedStudent(null);
 				}}
--- src/components/templates/Dashboard/WinnersUnit.tsx
+++ src/components/templates/Dashboard/WinnersUnit.tsx
@@ -1,12 +1,54 @@
 "use client";
 
-import { StatCard } from "@/components/molecules";
-import { WinnerFilter, WinnerLists } from "@/components/organisms/Dashboard/Winners";
+import { QueryTabOption, StatCard } from "@/components/molecules";
+import { WinnerLists } from "@/components/organisms/Dashboard/Winners";
+import { useApiClient } from "@/libes/hooks";
+import { AdminStudentListItem, AdminStudentStatsData } from "@/libes/interface/registration";
+import { updateURLSearchParams } from "@/utils/helpers/url.helpers";
 import { CheckCircle2, PenLine, RefreshCw } from "lucide-react";
-import { useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export const WinnersUnit = () => {
-	const [activeTab, setActiveTab] = useState("All");
+	const [stats, setStats] = useState<AdminStudentStatsData | null>(null);
+	const [registrations, setRegistrations] = useState<AdminStudentListItem[]>([]);
+	const { get } = useApiClient();
+	const searchParams = useSearchParams();
+	const query = Object.fromEntries(searchParams.entries());
+
+	const { status } = query;
+
+	const getStudentStats = async () => {
+		try {
+			const { data, status } = await get("API_URL", "admin/student-stats");
+			if (status === 200) {
+				setStats(data?.data);
+			}
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	const getStudentsList = async () => {
+		if (status === "All") delete query?.status;
+		let optionalParams: any = {};
+		const params = updateURLSearchParams(query, optionalParams);
+		try {
+			const { data, status } = await get("API_URL", `admin/students?${params}`);
+			if (status === 200) {
+				setRegistrations(data?.data?.items);
+			}
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	useEffect(() => {
+		getStudentsList();
+	}, [status]);
+	useEffect(() => {
+		getStudentStats();
+	}, []);
 	return (
 		<section>
 			<div className="mb-6 flex gap-4">
@@ -15,10 +57,17 @@ export const WinnersUnit = () => {
 				<StatCard title="Awarded" value="1,517" change="+19% from last month" icon={CheckCircle2} />
 			</div>
 
-			<WinnerFilter activeTab={activeTab} setActiveTab={setActiveTab} tabs={["All", "Yet_to_Award", "Awarded"]} />
+			<QueryTabOption
+				filterKey="status"
+				tabsOption={[
+					{ label: "All", id: "All" },
+					{ label: "Yet to Award", id: "Approved" },
+					{ label: "Awarded", id: "Awarded" },
+				]}
+			/>
 
 			<div className="rounded-lg border border-border bg-card  shadow-sm">
-				<WinnerLists />
+				<WinnerLists data={registrations} updateData={getStudentsList} />
 			</div>
 		</section>
 	);
